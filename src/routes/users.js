4f8d7ab84487c786c1ce020f875f2b645b1588a3
yuchaoWu <402939985@qq.com>
2020-07-16 10:31:47 +0800

    add :add code

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,6 @@
 const router = require('koa-router')()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../conf/constants')
 
 router.prefix('/users')
 
@@ -8,6 +10,37 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  let userInfo
+  if (userName === 'zhangsan' && password === 'abc') {
+    userInfo = {
+      userId: 1,
+      userName: 'zhangsan',
+      nickName: '张三',
+      gender: 1
+    }
+  }
+  // 加密 userinfo
+  let token
+  if (userInfo) {
+    token = jwt.sign(userInfo, SECRET, { expiresIn: '1h' })
+  }
+
+
+  if (userInfo == null) {
+    ctx.body = {
+      error: -1,
+      msg: '登录失败'
+    }
+    return
+  }
+  ctx.body = {
+    error: 0,
+    data: token
+  }
 })
 
 module.exports = router
